f0119a3dc9bb50c262d505bf2da1e72f45813784
bverjus <119845974+bverjus@users.noreply.github.com>
2024-09-09 09:52:50 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,8 @@ module.exports = {
     './inc/**/*.php',
     './templates/**/*.php',
     './safelist.txt'
+    './**/*.php',
+    "./**/*.js",
     //'./**/*.php', // recursive search for *.php (be aware on every file change it will go even through /node_modules which can be slow, read doc)
   ],
   safelist: [
@@ -14,7 +16,50 @@ module.exports = {
     //}
   ],
   theme: {
-    extend: {}
+    extend: {
+      screens: {
+        xs: "320px",
+        sm: "460px",
+        md: "768px",
+        medium: "1125px",
+        base: "1250px",
+        lg: "1440px",
+        xlarge: "1680px",
+        xl: "1920px",
+        "2xl": "2300px",
+      },
+      fontFamily: {
+        'title': ["Playfair Display", 'serif'],
+        'text': ['Open Sans', 'sans-serif'],
+      },
+
+      fontSize:{
+        'secondary_title': ['48px',{
+          fontFamily: "Playfair Display",
+          lineHeight: '63.98px',
+          fontWeight: '400',
+          fontStyle: 'normal',
+        }],
+
+        'primary_title': ['64px',{
+          fontFamily: "Playfair Display",
+          lineHeight: '85.31px',
+          fontWeight: '400',
+          fontStyle: 'normal',
+        }],
+
+          },
+
+      colors: { 
+        'primary-color': '#A97C3B',
+        'secondary-color': '#7F1110',
+      },
+
+      margin: {
+        'large' : '150px',
+        'mobile': '80px',
+     } 
+    }
   },
   plugins: []
 }
